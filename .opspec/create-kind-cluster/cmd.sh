3c55be6b0622721b7ecc1a58347ce104ccf84915
zachpuck <zachpuckett@gmail.com>
2018-11-26 10:29:32 -0800

    add kind op

---
+++ .opspec/create-kind-cluster/cmd.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+echo "installing git and docker"
+apk update && apk -U add git docker
+apk add openrc --no-cache
+
+echo "installing kind (kubernetes in docker)"
+cd /go/src/
+go get sigs.k8s.io/kind
+
+echo "create cluster"
+kind create cluster
