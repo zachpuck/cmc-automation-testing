bb2a9f4782636fef7adbf2e86ba025ebe1bc4984
zachpuck <zachpuckett@gmail.com>
2019-02-20 10:41:37 -0800

    test script

---
+++ script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+ function finish {
+  kind delete cluster
+}
+trap finish EXIT
+
+ echo "The current environment contains these variables: $(env)"
+echo "The current directory is $(pwd)"
+
+ service docker start
+sleep 10
+kind create cluster --wait=10m --loglevel=debug
+
+ export KUBECONFIG=$(kind get kubeconfig-path)
+
+ kubectl create clusterrolebinding superpowers --clusterrole=cluster-admin --user=system:serviceaccount:kube-system:default
+
+ echo "The current pods are:"
+kubectl get pods --all-namespaces
+kubectl describe pods --all-namespaces
+
+ echo $HELM_HOST
+# expects tillerless helm, since HELM_HOST is defined
+helm plugin install https://github.com/rimusz/helm-tiller || true
+helm tiller start-ci
+
+ # install cert-manager (required by cma-aws chart)
+# installing cert-manager and ingress, but using NodePort for CI
+# Note: removed --wait, it times out downloading the .tgz file
+kubectl apply \
+    -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+kubectl create namespace cert-manager
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+helm repo update
+helm install --name cert-manager --namespace cert-manager stable/cert-manager --debug
+sleep 10
+helm install --name nginx-ingress stable/nginx-ingress --debug
